8881ebe1fa6d53f62d96854fc48645c4173d39eb
Serg <47279391+s-evg@users.noreply.github.com>
2021-06-11 21:18:03 +0500

    Update configuration.py
    default

--- configuration.py
+++ configuration.py
@@ -1,5 +1,3 @@
-# vk_token = '900f8474ea733598a0d23659e9fb1f304fcadafe96b546bed41257e32038f15d90187d9d805770a68ab22' #my token
-vk_token = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008' #toten_begemot
-user_id = '15424191'
+vk_token = 'YOR VK TOKEN' #my token
 version = '5.131'
-ya_token = 'AQAAAAADc5TwAADLW3ZHvp0fBUjWse7hni038tw'
+ya_token = 'YOR YandexDisk TOKEN'

4621df6f127f94e2403c797327df14269bdd1768
s-evg <s-evg@bk.ru>
2021-06-11 21:15:51 +0500

    Initial commit

---
+++ configuration.py
@@ -0,0 +1,5 @@
+# vk_token = '900f8474ea733598a0d23659e9fb1f304fcadafe96b546bed41257e32038f15d90187d9d805770a68ab22' #my token
+vk_token = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008' #toten_begemot
+user_id = '15424191'
+version = '5.131'
+ya_token = 'AQAAAAADc5TwAADLW3ZHvp0fBUjWse7hni038tw'
